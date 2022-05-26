e6580d89751f6939ad7f9801f25461429b601aeb
Vhruzin387 <vhruzin387@technikumkreatywne.pl>
2022-05-26 10:42:06 +0200

    v1.0

---
+++ BinarneWuys/BinarneWuys/BinarneWuys.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+
+    int wyszukaj(double* lista, int n, double szukana) {
+        int L = 0, P = n - 1, sr = 0;
+        while (L <= P) {
+            sr = (L + P) / 2;
+            if (lista[sr] == szukana)
+                return sr;
+            if (lista[sr] > szukana)
+                P = sr - 1;
+            else
+                L = sr + 1;
+        }
+        return -1;
+    }
+
+
